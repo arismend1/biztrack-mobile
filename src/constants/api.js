29f6a78ab3935d18c2512c409e21a49f033cb728
arismend1 <anglorangzam@gmail.com>
2026-01-12 01:57:21 -0500

    add file

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = 'https://biztrack-backend-eebz.onrender.com/api';
+// Para Android Emulator use 10.0.2.2, para iOS use localhost
+export const API_BASE_URL = 'http://10.0.2.2:3000/api';
+// export const API_BASE_URL = 'https://biztrack-backend-eebz.onrender.com/api';
 
 export const ENDPOINTS = {
     LOGIN: '/auth/login',
